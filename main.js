70c82f49cf51fc7a82877f872554a1f9abfdf301
VitFernandes <111156572+VitFernandes@users.noreply.github.com>
2022-12-16 18:08:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+
+// Create canvas variable
+ blockY=1;
+ blockX=1;
+ var canvas = new fabric.Canvas('myCanvas');
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage)
+{
+	// para enviar imagens
+	fabric.Image.fromURL(get_image, function(Img)
+	{
+	block_image_object = Img;
+
+	block_image_object.sacaleToWidth(block_image_width);
+	block_image_object.sacaleToHeight(block_image_height);
+	block_image_object.set({
+	top:block_y,
+	left:block_x
+	});
+	canvas.add(block_image_object);
+});
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // adicione os códigos adequados às teclas
+	{
+		// enviar ranger escarlate
+		new_image('rr1.png');
+		console.log("e");
+	}
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		// enviar ranger verde
+		new_image('gr.png');
+		console.log("v");
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		// enviar ranger amarelo
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		// enviar ranger rosa
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+	// enviar ranger índigo
+	}
+	
+}
+
